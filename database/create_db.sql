ddcc349eedf992aa70142bc27946f59d48feb73e
mva-one <marcell@mva-one.de>
2022-05-19 17:59:55 +0200

    modified column names

--- database/create_db.sql
+++ database/create_db.sql
@@ -1,31 +1,35 @@
-CREATE TABLE IF NOT EXISTS gctl_node (
+DROP TABLE IF EXISTS gctl_sensor;
+DROP TABLE IF EXISTS gctl_actor;
+DROP TABLE IF EXISTS gctl_node;
+
+CREATE TABLE gctl_node (
     nid     SERIAL PRIMARY KEY,
     host    VARCHAR(100) NOT NULL,
     descr   VARCHAR(100)
 );
 
-CREATE TABLE IF NOT EXISTS gctl_actor (
+CREATE TABLE gctl_actor (
     aid     SERIAL PRIMARY KEY,
     node    INTEGER REFERENCES gctl_node(nid),
     atype   CHAR NOT NULL,
     gpio    NUMERIC(2),
-    astate  BOOLEAN NOT NULL DEFAULT FALSE,
-    invert  BOOLEAN NOT NULL DEFAULT FALSE,
-    locked  BOOLEAN NOT NULL DEFAULT FALSE,
-    aenable BOOLEAN NOT NULL DEFAULT TRUE,
+    stat    BOOLEAN NOT NULL DEFAULT FALSE,
+    inv     BOOLEAN NOT NULL DEFAULT FALSE,
+    lock    BOOLEAN NOT NULL DEFAULT FALSE,
+    enab    BOOLEAN NOT NULL DEFAULT TRUE,
     descr   VARCHAR(100),
     CONSTRAINT actor_type CHECK (atype IN ('V', 'P', 'L'))
 );
 
-CREATE TABLE IF NOT EXISTS gctl_sensor (
+CREATE TABLE gctl_sensor (
     sid         SERIAL PRIMARY KEY,
     node        INTEGER REFERENCES gctl_node(nid),
     stype       CHAR NOT NULL,
-    svalue      REAL,
+    val         REAL,
     gpio        NUMERIC(2),
-    refresh_int INTEGER NOT NULL DEFAULT 60,
-    senable     BOOLEAN NOT NULL DEFAULT TRUE,
+    refr        INTEGER NOT NULL DEFAULT 60,
+    enab        BOOLEAN NOT NULL DEFAULT TRUE,
     descr       VARCHAR(100),
     CONSTRAINT sensor_type CHECK (stype IN ('F', 'R', 'H', 'L')),
-    CONSTRAINT refresh_int_positive CHECK (refresh_int > 0)
+    CONSTRAINT refresh_int_positive CHECK (refr > 0)
 );
