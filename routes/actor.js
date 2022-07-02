82d57e0f824c1bd990c6ece498dcabd035a26749
mva-one <marcell@mva-one.de>
2022-07-02 11:58:35 +0200

    changed for docker

--- routes/actor.js
+++ routes/actor.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    const response = await fetch('http://localhost:3000/actor');
+    const response = await fetch('http://postgrest:3000/actor');
     const actors = await response.json();
     res.render('actors', {title: 'Actors - Gctl ALPHA', actors} );
 });
--- routes/node.js
+++ routes/node.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    const response = await fetch('http://localhost:3000/node');
+    const response = await fetch('http://postgrest:3000/node');
     const nodes = await response.json();
     res.render('nodes', {title: 'Nodes - Gctl ALPHA', nodes} );
 });
